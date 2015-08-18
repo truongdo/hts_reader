eb92679733afe192fa58e2d3e4fa0edf07160cef
truongdq <truongdq54@gmail.com>
2015-08-18 21:20:54 +0900

    Fix bugs

--- hts_reader/dec_tree.py
+++ hts_reader/dec_tree.py
@@ -105,12 +105,12 @@ class DecisionTree:
                 if ctx in cache:
                     logger.info("return cached")
                     return cache[cache_id][ctx]
-                else:
-                    raise Exception("cache has key " + str(sid) + "-" + str(stid) + ". But no model found. Some bugs exits")
-            else:
-                model = self.trees[(sid, stid)].parse(ctx, self.qs_list[type])
-                cache[cache_id] = {ctx: model}
-                return model
+                # else:
+                    # raise Exception("cache has key " + str(sid) + "-" + str(stid) + ". But no model found. Some bugs exits")
+            # else:
+            model = self.trees[(sid, stid)].parse(ctx, self.qs_list[type])
+            cache[cache_id] = {ctx: model}
+            return model
 
     def parse_has_answer(self, ctx, answer, sid=None, stid=None, type="mgc"):
         """
